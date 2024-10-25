c83b8df2902635b89bf57f24c8f743eca2934c33
Will Southerland <144608338+southerlandw100@users.noreply.github.com>
2024-10-25 09:41:00 -0400

    Update main.py
    Includes textract_engine.py, commented out in order to not use up free API calls

--- src/main.py
+++ src/main.py
@@ -2,20 +2,22 @@ import os
 import cv2
 from image_preprocessing import rescale_image, rotate_image, deskew_image, remove_noise, remove_shadows, grayscale_image
 from tesseract_engine import run_tesseract
+# from textract_engine import run_textract  # Textract commented out for now
 
 # Enhances the image by applying rescaling, rotation, deskewing, noise removal, and shadow removal.
-def enhance_image(img, tmp_path, rotate=True):
-    img = rescale_image(img)
+def enhance_image(img, tmp_path, output_dir, rotate=True):
+    img = rescale_image(img, output_dir=output_dir)
 
     if rotate:
+        tmp_path = os.path.join(output_dir, "rotated.jpg")
         cv2.imwrite(tmp_path, img)
         rotate_image(tmp_path, tmp_path)
         img = cv2.imread(tmp_path)
 
-    img = deskew_image(img)
-    img = remove_shadows(img)
-    img = grayscale_image(img)
-    img = remove_noise(img)
+    img = remove_shadows(img, output_dir=output_dir)        
+    img = deskew_image(img, output_dir=output_dir) 
+    img = grayscale_image(img, output_dir=output_dir)
+    img = remove_noise(img, output_dir=output_dir)
 
     return img
 
@@ -28,12 +30,18 @@ def process_receipt(filename, output_dir, rotate=True):
         return
 
     tmp_path = os.path.join(output_dir, "tmp_processed.jpg")
-    img = enhance_image(img, tmp_path, rotate)
+    img = enhance_image(img, tmp_path, output_dir, rotate)
 
     cv2.imwrite(tmp_path, img)
 
-    output_path = os.path.join(output_dir, filename.split(".")[0] + ".txt")
+    output_path = os.path.join(output_dir, filename.split("testing.")[0] + ".txt")
     run_tesseract(tmp_path, output_path)
+
+    # Textract OCR, left commented out for now until QC logic is implemented
+    # textract_output_path = os.path.join(output_dir, filename.split(".")[0] + "_textract.txt")
+    # textract_text = run_textract(tmp_path)
+    # with open(textract_output_path, "w", encoding='utf-8') as out:
+    #     out.write(textract_text)
 
 # Main function to process the image based on user input.
 def main():
