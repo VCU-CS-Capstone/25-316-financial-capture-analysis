61177379cd231dec185117457ecaa17b1c378053
Josh Taro <150681748+tarojosh@users.noreply.github.com>
2024-11-15 07:42:04 -0500

    Able to grab information from DynamoDB and display it as a table in Receipts tab.

--- Notes and Research/ReactJS/src/Dashboard.js
+++ Notes and Research/ReactJS/src/Dashboard.js
@@ -1,20 +1,75 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
 import { DateRangePicker } from 'rsuite';
-import './App.css';
+import './DashboardChart/Dashboard.css';
+import './DashboardChart/expenseDonutChart.js';
 import Dropdown from 'react-dropdown';
 
-const options = [
-    'Dep1', 'Dep2', 'Dep3'
-];
 
 const Dashboard = () => {
+    const chartRef = useRef(null);
+    const myChartRef = useRef(null);
+
+    useEffect(() => {
+        if (chartRef.current) {
+            const ctx = chartRef.current.getContext('2d');
+
+            if (myChartRef.current) {
+                myChartRef.current.destroy();
+            }
+
+            myChartRef.current = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow'],
+                    datasets: [{
+                        label: 'Dataset',
+                        data: [300, 50, 100],
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)',
+                            'rgb(255, 205, 86)'
+                        ],
+                        hoverOffset: 4
+                    }]
+                },
+                options: {
+                    cutout: '60%',
+                    plugins: {
+                        legend: {
+                            position: 'left'
+                        }
+                    }
+                }
+            });
+        }
+
+        return () => {
+            if (myChartRef.current) {
+                myChartRef.current.destroy();
+            }
+        };
+    }, []);
+
     return (
         <div>
             <h1 className='Headings'>Dashboard</h1>
             <DateRangePicker className='Subheading' />
-        </div>
-    )
-        
+            <div className='flexContainer'>
 
+                <div className='BodyContainer BodyContainer-first shadow roundBorder'>
+                    Total Expenses
+                    <div style={{ width: '300px' }}>
+                        <canvas id='donutchart' ref={chartRef}></canvas>
+                    </div>
+                </div>
+    
+                <div className='BodyContainer BodyContainer-second shadow roundBorder'>
+                    Placeholder
+                </div>
+            
+            </div>
+        </div>
+    );
 };
 export default Dashboard;
--- Notes and Research/ReactJS/src/Receipts.js
+++ Notes and Research/ReactJS/src/Receipts.js
@@ -1,20 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DateRangePicker } from 'rsuite';
 import './App.css';
-import Dropdown from 'react-dropdown';
 
-const options = [
-    'Dep1', 'Dep2', 'Dep3'
-];
+import './DashboardChart/Dashboard.css';
 
 const Receipts = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://fryt5r9woh.execute-api.us-east-1.amazonaws.com/items');
+                
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+  
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+  
+        fetchData();
+    }, []);
+  
+    if (loading) return <p className='BodyContainer BodyContainer-first shadow roundBorder'>Loading...</p>;
+    if (error) return <p className='BodyContainer BodyContainer-first shadow roundBorder'>Error: {error}</p>;
+
     return (
         <div>
             <h1 className='Headings'>Receipts</h1>
             <DateRangePicker className='Subheading' />
-        </div>
-    )
-        
+            <div className='BodyContainer BodyContainer-wide shadow roundBorder'>
+                <table >
+                    <tr className='roundBorder'>
+                        <th>Date</th>
+                        <th>Total Amount</th>
+                        <th>Total Items</th>
+                        <th>Vendor</th>
+                        <th>Address</th>
+                        <th>Image</th>
+                        <th>Expense</th>
+                    </tr>
+                    <tbody>
+                    {data.sort((a, b) => new Date(b.Date) - new Date(a.Date)).map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.Date}</td>
+                            <td>{item.TotalAmount}</td>
+                            <td>{item.TotalItems}</td>
+                            <td>{item.VendorName}</td>
+                            <td>{item.VendorAddress}</td>
+                            <td>{item.ImageURL}</td>
+                            <td>{item.ExpenseType}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
 
+
+                {/* <ul>
+                    {data.map((item, index) => (
+                        <li key={index}>{item.SK}</li>
+                    ))}
+                </ul> */}
+            </div>
+        </div>
+    );
 };
+
 export default Receipts;
