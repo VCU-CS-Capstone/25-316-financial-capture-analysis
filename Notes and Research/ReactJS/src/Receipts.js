4c9b1222f8a66929fd3f1940c7050cb73085600f
Josh Taro <150681748+tarojosh@users.noreply.github.com>
2024-11-21 16:58:57 -0500

    Receipt page is now able to sort receipts based on the date range picker.

--- Notes and Research/ReactJS/src/Receipts.js
+++ Notes and Research/ReactJS/src/Receipts.js
@@ -1,14 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { DateRangePicker } from 'rsuite';
 import './App.css';
-
 import './DashboardChart/Dashboard.css';
+import 'rsuite/dist/rsuite.min.css';
 
 const Receipts = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [dateRange, setDateRange] = useState([null, null]);
   
+    const handleDateChange = (range) => {
+        setDateRange(range);
+    
+        if (range && range.length === 2) {
+            const [startDate, endDate] = range;
+
+            const isValidStart = startDate instanceof Date && !isNaN(startDate);
+            const isValidEnd = endDate instanceof Date && !isNaN(endDate);
+    
+            if (isValidStart && isValidEnd) {
+                console.log("Start Date:", startDate, "End Date:", endDate);
+            }
+            else {
+                console.log("Reset sorting back to default");
+            }
+        }
+    };
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -37,39 +56,49 @@ const Receipts = () => {
     return (
         <div>
             <h1 className='Headings'>Receipts</h1>
-            <DateRangePicker className='Subheading' />
+            <DateRangePicker showOneCalendar size="sm" format="yyyy/MM/dd" className='Subheading' onChange={handleDateChange}/>
             <div className='BodyContainer BodyContainer-wide shadow roundBorder'>
                 <table >
-                    <tr className='roundBorder'>
+                    <thead className='roundBorder'>
                         <th>Date</th>
                         <th>Total Amount</th>
                         <th>Total Items</th>
-                        <th>Vendor</th>
+                        <th>Merchant</th>
                         <th>Address</th>
                         <th>Image</th>
-                        <th>Expense</th>
-                    </tr>
+                        <th>Category</th>
+                    </thead>
                     <tbody>
-                    {data.sort((a, b) => new Date(b.Date) - new Date(a.Date)).map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.Date}</td>
-                            <td>{item.TotalAmount}</td>
-                            <td>{item.TotalItems}</td>
-                            <td>{item.VendorName}</td>
-                            <td>{item.VendorAddress}</td>
-                            <td>{item.ImageURL}</td>
-                            <td>{item.ExpenseType}</td>
-                        </tr>
-                        ))}
+                        {data.filter(
+                            item => {
+                                if (dateRange && dateRange.length === 2) {
+                                    // Get the start and end date of the date-range-picker
+                                    const [startDate, endDate] = dateRange;
+                                    const isValidStart = startDate instanceof Date && !isNaN(startDate);
+                                    const isValidEnd = endDate instanceof Date && !isNaN(endDate);
+
+                                    // We only apply the range to the table if the user has a defined date range (start and end) set
+                                    // As well as making sure it is a valid date (no empty values).
+                                    if (isValidStart && isValidEnd) {
+                                        const itemDate = new Date(item.Date);
+                                        return itemDate >= startDate && itemDate <= endDate;
+                                    }
+                                }
+                                return true;
+                            }).sort((a, b) => new Date(b.Date) - new Date(a.Date))
+                            .map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.Date}</td>
+                                <td>{item.TotalAmount}</td>
+                                <td>{item.TotalItems}</td>
+                                <td>{item.VendorName}</td>
+                                <td>{item.VendorAddress}</td>
+                                <td>{item.ImageURL}</td>
+                                <td>{item.ExpenseType}</td>
+                            </tr>
+                            ))}
                     </tbody>
                 </table>
-
-
-                {/* <ul>
-                    {data.map((item, index) => (
-                        <li key={index}>{item.SK}</li>
-                    ))}
-                </ul> */}
             </div>
         </div>
     );
