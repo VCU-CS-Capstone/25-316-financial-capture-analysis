387167230ecd887621254dcf7defd4506514b321
Josh Taro <150681748+tarojosh@users.noreply.github.com>
2025-02-26 09:26:35 -0500

    Updated TableBody.js to account for non-numeric values

--- src/SortableTable/TableBody.js
+++ src/SortableTable/TableBody.js
@@ -6,16 +6,20 @@ const TableBody = ({ tableData, columns, openModal }) => {
                     <tr key={index}>
                         {columns.map(({ accessor }) => {
                             if (accessor === "TotalAmount") {
-                                const tData = data[accessor] ? data[accessor] : "——";
+                                let tData = parseFloat(data[accessor]);
+                            
+                                // Non-numeric values will default to $0.00
+                                if (isNaN(tData)) { tData = 0; }
+                            
                                 return (
                                     <td key={accessor}>
-                                        {(tData || 0).toLocaleString("en-US", {
+                                        {tData.toLocaleString("en-US", {
                                             style: "currency",
                                             currency: "USD",
                                         })}
                                     </td>
                                 );
-                            }
+                            }                            
 
                             if (accessor === "Actions") {
                                 return (
